fe1c1994bc5ffa960ee7c69a73f130c2418b2b55
runsf <mclausaudio@gmail.com>
2018-05-14 21:50:41 +0000

    added tuner and coundown jsx

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,27 +1,31 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-
-
-var Nav = React.createClass({
-    render: function() {
-        return (
-            <div className="top-bar">
-              <div className="top-bar-left">
-                <ul className="dropdown menu" data-dropdown-menu>
-                  <li className="menu-text">React Timer</li>
-                  <li><a href="#">One</a></li>
-                  <li><a href="#">Two</a></li>
-                </ul>
-              </div>
-              <div className="top-bar-right">
-                <ul className="menu">
-                  <li className="menu-text">Created by <a href="" target="_blank">Michael Claus</a></li>
-                </ul>
-              </div>
-            </div>
-            );
-    }
-});
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/mclausaudio" target="_blank">Michael Claus</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 module.exports = Nav;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+
+
+
+var Timer = React.createClass({
+    render: function() {
+        return <p>timer.jsx</p>
+
+    }
+});
+
+module.exports = Timer;
