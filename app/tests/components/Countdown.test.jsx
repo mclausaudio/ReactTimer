59434c5f9391b3b60aee11ec9a4113b75953e481
runsf <mclausaudio@gmail.com>
2018-05-21 17:28:05 +0000

    finalize test and add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -25,7 +25,7 @@ describe('handleSetCountdown', () => {
             done();
         }, 1000)
     });
-        it('should never have count go below 0', (done) => {
+    it('should never have count go below 0', (done) => {
         var countdown = TestUtils.renderIntoDocument(<Countdown/>);
         countdown.handleSetCountdown(1);
         
@@ -34,4 +34,26 @@ describe('handleSetCountdown', () => {
             done();
         }, 4000)
     });
-});
+     it('should pause countdown on paused status', (done) => {
+       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+       countdown.handleSetCountdown(3);
+       countdown.handleStatusChange('paused');
+
+       setTimeout(() => {
+         expect(countdown.state.count).toBe(3);
+         expect(countdown.state.countdownStatus).toBe('paused');
+         done();
+       }, 1001);
+     });
+     it('should reset count to 0 on stopped status', (done) => {
+       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+       countdown.handleSetCountdown(3);
+       countdown.handleStatusChange('stopped');
+
+       setTimeout(() => {
+         expect(countdown.state.count).toBe(0);
+         expect(countdown.state.countdownStatus).toBe('stopped');
+         done();
+       }, 1001);
+     });     
+}); 
